11ed0da5465669ecceca07663baa126cba2f04bc
Alexandro Sas <alexandro.sas98@gmail.com>
2024-06-07 18:39:54 +0200

    hide

--- sw.js
+++ sw.js
@@ -1,47 +1,45 @@
-const CACHE_NAME = 'house-storage-v1';
-const urlsToCache = [
-  '/House-Storage/',
-  '/House-Storage/index.html',
-  '/House-Storage/css/styles.css'
-  // Add more URLs as needed
-];
+// const CACHE_NAME = 'house-storage-v1';
+// const urlsToCache = [
+//   '/House-Storage/index.html',
+//   '/House-Storage/css/styles.css'
+// ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(function(error) {
-        console.error('Failed to cache resources:', error);
-      })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//       .catch(function(error) {
+//         console.error('Failed to cache resources:', error);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response; // Return the cached response if found
-        }
-        return fetch(event.request); // Fetch from network if not in cache
-      })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response; // Return the cached response if found
+//         }
+//         return fetch(event.request); // Fetch from network if not in cache
+//       })
+//   );
+// });
 
-self.addEventListener('activate', function(event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName); // Delete old caches
-          }
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function(event) {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName); // Delete old caches
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
